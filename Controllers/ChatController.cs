c25ecf3d14262739e629f97f46d88ca04e4784b4
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-05-08 11:48:42 -0400

    added friendlist

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ChatManager.Models;
 
 namespace ChatManager.Controllers
 {
     public class ChatController : Controller
     {
-        // GET: Chat
+        int SelectedFriendId 
+        {
+            get
+            {
+                if (Session["selectedFriendId"] == null)
+                    Session["selectedFriendId"] = -1;
+                return (int)Session["selectedFriendId"];
+            }
+            set => Session["selectedFriendId"] = value;
+        }
+
+        [OnlineUsers.UserAccess]
         public ActionResult Index()
         {
+            ViewBag.User = OnlineUsers.GetSessionUser();
+            ViewBag.SelectedUserId = Session["selectedUserId"];
             return View();
         }
     }
